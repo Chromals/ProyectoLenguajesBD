74b9865a644bdc1e4287b4e7f6a10572f3bb57f8
jdaniel300 <134084447+jdaniel300@users.noreply.github.com>
2024-08-19 22:57:19 -0600

    Update CRUD_PROVEEDOR.sql
    trigger Proveedor_Audit

--- Services/ScriptBD/CRUD_PROVEEDOR.sql
+++ Services/ScriptBD/CRUD_PROVEEDOR.sql
@@ -1,3 +1,32 @@
+CREATE TABLE Proveedor_Audit (
+    Audit_ID NUMBER GENERATED ALWAYS AS IDENTITY,
+    Audit_Action VARCHAR2(10),
+    ID_Proveedor NUMBER,
+    Nombre VARCHAR2(50),
+    Apellido_1 VARCHAR2(50),
+    Apellido_2 VARCHAR2(50),
+    Telefono NUMBER,
+    Correo VARCHAR2(100),
+    ID_Direccion NUMBER,
+    Audit_Timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE OR REPLACE TRIGGER trg_Proveedor_Audit
+AFTER INSERT OR UPDATE OR DELETE ON Proveedor
+FOR EACH ROW
+BEGIN
+    IF INSERTING THEN
+        INSERT INTO Proveedor_Audit (Audit_Action, ID_Proveedor, p_Nombre, p_Apellido_1, p_Apellido_2, p_Telefono, p_Correo, ID_Direccion)
+        VALUES ('INSERT', :NEW.ID_Proveedor, :NEW.Nombre, :NEW.Apellido_1, :NEW.Apellido_2, :NEW.Telefono, :NEW.Correo, :NEW.ID_Direccion);
+    ELSIF UPDATING THEN
+        INSERT INTO Proveedor_Audit (Audit_Action, ID_Proveedor, p_Nombre, p_Apellido_1, p_Apellido_2, p_Telefono, p_Correo, ID_Direccion)
+        VALUES ('UPDATE', :OLD.ID_Proveedor, :NEW.Nombre, :NEW.Apellido_1, :NEW.Apellido_2, :NEW.Telefono, :NEW.Correo, :NEW.ID_Direccion);
+    ELSIF DELETING THEN
+        INSERT INTO Proveedor_Audit (Audit_Action, ID_Proveedor, p_Nombre, p_Apellido_1, p_Apellido_2, p_Telefono, p_Correo, ID_Direccion)
+        VALUES ('DELETE', :OLD.ID_Proveedor, :OLD.Nombre, :OLD.Apellido_1, :OLD.Apellido_2, :OLD.Telefono, :OLD.Correo, :OLD.ID_Direccion);
+    END IF;
+END trg_Proveedor_Audit;
+/
+
 CREATE OR REPLACE PACKAGE CRUD_PROVEEDOR AS
     PROCEDURE Insert_Proveedor(p_Nombre IN VARCHAR2, p_Apellido_1 IN VARCHAR2, p_Apellido_2 IN VARCHAR2, p_Telefono IN NUMBER, p_Correo IN VARCHAR2, p_ID_Direccion IN NUMBER, p_Success OUT NUMBER);
     PROCEDURE Update_Proveedor(p_ID_Proveedor IN NUMBER, p_Nombre IN VARCHAR2, p_Apellido_1 IN VARCHAR2, p_Apellido_2 IN VARCHAR2, p_Telefono IN NUMBER, p_Correo IN VARCHAR2, p_ID_Direccion IN NUMBER, p_Success OUT NUMBER);
