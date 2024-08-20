4f871c1ff4ad3ea175429877f2997ada48bae79f
jdaniel300 <134084447+jdaniel300@users.noreply.github.com>
2024-08-19 22:43:51 -0600

    Update CRUD_CLIENTES.sql
    trigger cliente_auditoria

--- Services/ScriptBD/CRUD_CLIENTES.sql
+++ Services/ScriptBD/CRUD_CLIENTES.sql
@@ -1,3 +1,32 @@
+CREATE TABLE Cliente_Audit (
+    Audit_ID NUMBER GENERATED ALWAYS AS IDENTITY,
+    Audit_Action VARCHAR2(10),
+    ID_Cliente NUMBER,
+    Nombre VARCHAR2(50),
+    Apellido_1 VARCHAR2(50),
+    Apellido_2 VARCHAR2(50),
+    Telefono NUMBER,
+    Correo VARCHAR2(100),
+    ID_Direccion NUMBER,
+    Audit_Timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE OR REPLACE TRIGGER trg_Cliente_Audit
+AFTER INSERT OR UPDATE OR DELETE ON Cliente
+FOR EACH ROW
+BEGIN
+    IF INSERTING THEN
+        INSERT INTO Cliente_Audit (Audit_Action, ID_Cliente, p_Nombre, p_Apellido_1, p_Apellido_2, p_Telefono, p_Correo, ID_Direccion)
+        VALUES ('INSERT', :NEW.ID_Cliente, :NEW.Nombre, :NEW.Apellido_1, :NEW.Apellido_2, :NEW.Telefono, :NEW.Correo, :NEW.ID_Direccion);
+    ELSIF UPDATING THEN
+        INSERT INTO Cliente_Audit (Audit_Action, ID_Cliente, p_Nombre, p_Apellido_1, p_Apellido_2, p_Telefono, p_Correo, ID_Direccion)
+        VALUES ('UPDATE', :OLD.ID_Cliente, :NEW.Nombre, :NEW.Apellido_1, :NEW.Apellido_2, :NEW.Telefono, :NEW.Correo, :NEW.ID_Direccion);
+    ELSIF DELETING THEN
+        INSERT INTO Cliente_Audit (Audit_Action, ID_Cliente, p_Nombre, p_Apellido_1, p_Apellido_2, p_Telefono, p_Correo, ID_Direccion)
+        VALUES ('DELETE', :OLD.ID_Cliente, :OLD.Nombre, :OLD.Apellido_1, :OLD.Apellido_2, :OLD.Telefono, :OLD.Correo, :OLD.ID_Direccion);
+    END IF;
+END trg_Cliente_Audit;
+/
+
 CREATE OR REPLACE PACKAGE CRUD_CLIENTE AS
 
     -- Cliente procedures
@@ -9,7 +38,7 @@ CREATE OR REPLACE PACKAGE CRUD_CLIENTE AS
 
 END CRUD_CLIENTE;
 /
-
+    
 CREATE OR REPLACE PACKAGE BODY CRUD_CLIENTE AS
 
     PROCEDURE Insert_Cliente(p_Nombre IN VARCHAR2, p_Apellido_1 IN VARCHAR2, p_Apellido_2 IN VARCHAR2, p_Telefono IN NUMBER, p_Correo IN VARCHAR2, p_ID_Direccion IN NUMBER, p_Result OUT NUMBER) IS
