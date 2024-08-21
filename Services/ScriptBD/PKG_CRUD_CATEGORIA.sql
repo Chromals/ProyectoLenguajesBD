f71f79a092c419d3abd471d97fea4eb9afa503c9
DavidZ07 <dchavesd.07@gmail.com>
2024-08-21 00:13:07 -0600

    Arreglo nomenclatura
    Arreglo nomenclatura de archivos y nuevos paquetes

---
+++ Services/ScriptBD/PKG_CRUD_CATEGORIA.sql
@@ -0,0 +1,57 @@
+CREATE OR REPLACE PACKAGE CRUD_CATEGORIA AS
+
+    -- Categoría procedures
+    PROCEDURE Insert_Categoria(p_Nombre IN VARCHAR2, p_Result OUT VARCHAR2);
+    PROCEDURE Update_Categoria(p_ID_Categoria IN NUMBER, p_Nombre IN VARCHAR2, p_Result OUT VARCHAR2);
+    PROCEDURE Delete_Categoria(p_ID_Categoria IN NUMBER, p_Result OUT VARCHAR2);
+    PROCEDURE Select_All_Categoria(p_Result OUT SYS_REFCURSOR);
+    PROCEDURE Select_Categoria(p_ID_Categoria IN NUMBER, p_Result OUT SYS_REFCURSOR);
+
+END CRUD_CATEGORIA;
+/
+    
+CREATE OR REPLACE PACKAGE BODY CRUD_CATEGORIA AS
+
+    PROCEDURE Insert_Categoria(p_Nombre IN VARCHAR2, p_Result OUT VARCHAR2) IS
+    BEGIN
+        INSERT INTO Categoria (Nombre)
+        VALUES (p_Nombre);
+        p_Result := SQL%ROWCOUNT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            p_Result := 'Error: ' || SQLERRM;
+    END Insert_Categoria;
+
+    PROCEDURE Update_Categoria(p_ID_Categoria IN NUMBER, p_Nombre IN VARCHAR2, p_Result OUT VARCHAR2) IS
+    BEGIN
+        UPDATE Categoria
+        SET Nombre = p_Nombre
+        WHERE ID_Categoria = p_ID_Categoria;
+        p_Result := SQL%ROWCOUNT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            p_Result := 'Error: ' || SQLERRM;
+    END Update_Categoria;
+
+    PROCEDURE Delete_Categoria(p_ID_Categoria IN NUMBER, p_Result OUT VARCHAR2) IS
+    BEGIN
+        DELETE FROM Categoria
+        WHERE ID_Categoria = p_ID_Categoria;
+        p_Result := SQL%ROWCOUNT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            p_Result := 'Error: ' || SQLERRM;
+    END Delete_Categoria;
+
+    PROCEDURE Select_All_Categoria(p_Result OUT SYS_REFCURSOR) IS
+    BEGIN
+        OPEN p_Result FOR SELECT * FROM Categoria;
+    END Select_All_Categoria;
+
+    PROCEDURE Select_Categoria(p_ID_Categoria IN NUMBER, p_Result OUT SYS_REFCURSOR) IS
+    BEGIN
+        OPEN p_Result FOR SELECT * FROM Categoria WHERE ID_Categoria = p_ID_Categoria;
+    END Select_Categoria;
+
+END CRUD_CATEGORIA;
+/
---
+++ Services/ScriptBD/PKG_CRUD_PRODUCTO.sql
@@ -0,0 +1,61 @@
+CREATE OR REPLACE PACKAGE CRUD_PRODUCTO AS
+
+    -- Producto procedures
+    PROCEDURE Insert_Producto(p_Nombre IN VARCHAR2, p_Descripcion IN VARCHAR2, p_Precio IN NUMBER, p_ID_Categoria IN NUMBER, p_Cantidad IN NUMBER, p_Result OUT VARCHAR2);
+    PROCEDURE Update_Producto(p_ID_Producto IN NUMBER, p_Nombre IN VARCHAR2, p_Descripcion IN VARCHAR2, p_Precio IN NUMBER, p_ID_Categoria IN NUMBER, p_Cantidad IN NUMBER, p_Result OUT VARCHAR2);
+    PROCEDURE Delete_Producto(p_ID_Producto IN NUMBER, p_Result OUT VARCHAR2);
+    PROCEDURE Select_All_Producto(p_Result OUT SYS_REFCURSOR);
+    PROCEDURE Select_Producto(p_ID_Producto IN NUMBER, p_Result OUT SYS_REFCURSOR);
+
+END CRUD_PRODUCTO;
+/
+    
+CREATE OR REPLACE PACKAGE BODY CRUD_PRODUCTO AS
+
+    PROCEDURE Insert_Producto(p_Nombre IN VARCHAR2, p_Descripcion IN VARCHAR2, p_Precio IN NUMBER, p_ID_Categoria IN NUMBER, p_Cantidad IN NUMBER, p_Result OUT VARCHAR2) IS
+    BEGIN
+        INSERT INTO Producto (Nombre, Descripcion, Precio, ID_Categoria, cantidad)
+        VALUES (p_Nombre, p_Descripcion, p_Precio, p_ID_Categoria, p_Cantidad);
+        p_Result := SQL%ROWCOUNT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            p_Result := 'Error: ' || SQLERRM;
+    END Insert_Producto;
+
+    PROCEDURE Update_Producto(p_ID_Producto IN NUMBER, p_Nombre IN VARCHAR2, p_Descripcion IN VARCHAR2, p_Precio IN NUMBER, p_ID_Categoria IN NUMBER, p_Cantidad IN NUMBER, p_Result OUT VARCHAR2) IS
+    BEGIN
+        UPDATE Producto
+        SET Nombre = p_Nombre,
+            Descripcion = p_Descripcion,
+            Precio = p_Precio,
+            ID_Categoria = p_ID_Categoria,
+            cantidad = p_Cantidad
+        WHERE ID_Producto = p_ID_Producto;
+        p_Result := SQL%ROWCOUNT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            p_Result := 'Error: ' || SQLERRM;
+    END Update_Producto;
+
+    PROCEDURE Delete_Producto(p_ID_Producto IN NUMBER, p_Result OUT VARCHAR2) IS
+    BEGIN
+        DELETE FROM Producto
+        WHERE ID_Producto = p_ID_Producto;
+        p_Result := SQL%ROWCOUNT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            p_Result := 'Error: ' || SQLERRM;
+    END Delete_Producto;
+
+    PROCEDURE Select_All_Producto(p_Result OUT SYS_REFCURSOR) IS
+    BEGIN
+        OPEN p_Result FOR SELECT * FROM Producto;
+    END Select_All_Producto;
+
+    PROCEDURE Select_Producto(p_ID_Producto IN NUMBER, p_Result OUT SYS_REFCURSOR) IS
+    BEGIN
+        OPEN p_Result FOR SELECT * FROM Producto WHERE ID_Producto = p_ID_Producto;
+    END Select_Producto;
+
+END CRUD_PRODUCTO;
+/
